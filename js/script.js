53deea6ef93a78fba3136a1b86ae32d0293da7fb
Dandav2923 <daniele.emili23@gmail.com>
2022-01-10 17:01:11 +0100

    inserimento email

--- js/script.js
+++ js/script.js
@@ -1,4 +1,18 @@
 const app = new Vue ({
     el: '#app',
-    
+    data: {
+        email: ['']
+    },
+    created(){
+        axios
+            .get("https://flynn.boolean.careers/exercises/api/random/mail")
+
+            .then((response) =>{
+                this.email = response.data.response;
+                console.log(response);
+            })
+            .catch(function(error){
+                console.log(error);
+            });
+    },
 })
