f81a3e6f8674b417c5a2a2211825ae16e0460896
Dandav2923 <daniele.emili23@gmail.com>
2022-01-10 17:09:31 +0100

    bonus

--- js/script.js
+++ js/script.js
@@ -1,18 +1,26 @@
 const app = new Vue ({
     el: '#app',
     data: {
-        email: ['']
+        email: []
     },
     created(){
-        axios
-            .get("https://flynn.boolean.careers/exercises/api/random/mail")
-
-            .then((response) =>{
-                this.email = response.data.response;
-                console.log(response);
-            })
-            .catch(function(error){
-                console.log(error);
-            });
+        this.start()
     },
+    methods : {
+        start: function() {
+            for (let index = 0; index < 10; index++) {
+                axios
+                    .get("https://flynn.boolean.careers/exercises/api/random/mail")
+
+                    .then((response) => {
+                        this.email.push(response.data.response)
+                        console.log(response);
+                    })
+                    .catch(function (error) {
+                        console.log(error);
+                    });
+                
+            }
+        }
+    }
 })
